056479c81ed3f0a8940636f055491d6dcc0ee744
ravigoel08 <ravigoel.1997@gmail.com>
2019-11-10 11:23:52 +0530

    scraper

--- scr.py
+++ scr.py
@@ -13,7 +13,7 @@ f.write(headers)
 #fetching Container containing the data required by looping through each page
 for i in range(1,104):
     print('page ' +f'{i}'+' scraped')
-    my_url = f"https://www.magicbricks.com/mbsearch/propertySearch.html?propertyType_new=10001_10017&city=2624&searchType=1&propertyType=10001,10017&disWeb=Y&pType=10001,10017&category=S&groupstart=90&offset=0&maxOffset=4&attractiveIds=&page={i}&ltrIds=&preCompiledProp=&excludePropIds=&addpropertyDataSet="
+    my_url = f"https://www.magicbricks.com/property-for-sale/residential-real-estate?proptype=Residential-House,Villa&cityName=New-Delhi&page={i}"
     req = Request(my_url, headers={'User-Agent':'Mozilla/5.0'})
     uClient = urlopen(req).read()
     ucl = uClient.decode('utf-8')
@@ -31,8 +31,10 @@ for i in range(1,len(a)):
     price = (price.text).split()
     area = (area.text).split()
     if area[1] == 'sqm' or area[1] == 'sqyrd':
-        area[0] = str((int(area[0]))*Unit_map[f'{area[1]}'])
+        area[0] = (int(area[0]))*Unit_map[f'{area[1]}']
     if price[1] == 'Cr' or 'Lac' :
-        price[0] = (str(int(float(price[0]))*Unit_map[f'{price[1]}']))
-    f.write(price[0] + ", " + area[0] + "\n")
+        print(price[0])
+        #price[0] = (float(price[0]))*Unit_map[f'{price[1]}']
+        price[0] = int(price[0])
+   # f.write(str(price[0]) + ", " + str(area[0]) + "\n")
 f.close()
